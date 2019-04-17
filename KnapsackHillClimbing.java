ab06b3eeb9e850faa77eddb031864be6aee41a76
Colin Reilly <johndoe@example.com>
2019-04-17 16:00:47 +0200

    Works as well as before with csv parsing

--- KnapsackHillClimbing.java
+++ KnapsackHillClimbing.java
@@ -16,90 +16,123 @@ public class KnapsackHillClimbing {
     // static int solutionDef[] = { 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2 };
 
     final static String dataPath = "data/cable_12_100.csv";
-    static int weights[];
-    static int values[];
-    static int limit;
-    static int solutionSize;
+    final static int limit = 100;
+    static int solutionSize = 12;
+
+    // static int weights[];
+    // static int values[];
+    static List<List<Integer>> data;
     static int solutionDef[];
 
-    public static void finalPrint(int[] solution, int value) {
+    /***
+     * Prints the final values at the end of running this program.
+     * 
+     * @param solution - Array representing the items in the Knapsack.
+     * @param value    - Value of the items in the Knapsack.
+     */
+    public static void finalPrint(int[] solution, int value, List<List<Integer>> data) {
         int localBestWeight = 0;
 
         for (int i = 0; i < solution.length; i++) {
-            localBestWeight += solution[i] * weights[solutionDef[i]];
+            localBestWeight += solution[i] * data.get(i).get(1);
         }
 
         System.out.println("------------------");
+        System.out.println("final data " + data);
         System.out.println("final weight " + localBestWeight);
         System.out.println("final value " + value);
         System.out.println("final solution " + Arrays.toString(solution));
     }
 
+    /***
+     * Returns a 2D array representing the data held in a csv file.
+     * 
+     * Column 1: Value; Column 2: Weight.
+     * 
+     * @param path - Path of the csv file to load data from.
+     * @return 2D arrayList representing the data held in the csv file.
+     */
     public static List<List<Integer>> parseCSV(String path) {
         List<List<Integer>> records = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
+
             while ((line = br.readLine()) != null) {
                 String[] sValues = line.split(",");
+
                 Integer i1 = Integer.parseInt(sValues[0]);
                 Integer i2 = Integer.parseInt(sValues[1]);
                 Integer[] newRecord = { i1, i2 };
+
                 records.add(Arrays.asList(newRecord));
             }
         } catch (IOException e) {
             System.out.println("IOException thrown when trying to parse csv" + e);
         }
 
-        return(records);
+        return (records);
+    }
+
+    public static void printNeighborhood(int[][] neighborhood, int[][] neighborhoodData) {
+        System.out.println("neighborhood");
+
+        for (int i = 0; i < neighborhood[0].length; i++) {
+            System.out.println("n " + Arrays.toString(neighborhood[i]));
+            System.out.println("d " + Arrays.toString(neighborhoodData[i]));
+        }
     }
 
     public static void main(String[] args) {
-        List<List<Integer>> records = parseCSV(dataPath);
+        data = parseCSV(dataPath);
 
-        System.out.println(records);
-        // int solution[] = { 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 };
-        // int neighborhood[][] = new int[solutionSize][solutionSize];
-        // int localBestValue = 0;
-        // int neighborWeight = 0;
-        // int neighborValue = 0;
-        // int neighborhoodData[][] = new int[solutionSize][2]; // [2] is [weight,
-        // value]
-        // boolean solutionIsBest = false;
+        // Initialize variables
+        int solution[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        int neighborhood[][] = new int[solutionSize][solutionSize];
+        int localBestValue = 0;
+        int neighborWeight = 0;
+        int neighborValue = 0;
+        int neighborhoodData[][] = new int[solutionSize][2]; // [2] is [value, weight]
+        boolean solutionIsBest = false;
 
-        // for (int i = 0; i < solution.length; i++) {
-        // localBestValue += solution[i] * values[solutionDef[i]];
-        // }
+        // Get the initial "localBestValue" - the value of the initial solution
+        for (int i = 0; i < solution.length; i++) {
+            localBestValue += solution[i] * data.get(i).get(0);
+        }
 
-        // while (!solutionIsBest) {
-        // System.out.println("solution was not best");
+        while (!solutionIsBest) {
+            System.out.println(Arrays.toString(solution) + " solution was not best");
 
-        // for (int i = 0; i < solution.length; i++) {
-        // neighborhood[i] = solution.clone();
-        // neighborhood[i][i] = neighborhood[i][i] == 1 ? 0 : 1;
+            // Loop through all neighbors.
+            for (int i = 0; i < solution.length; i++) {
+                // Flip 1 bit for every solution in the neighborhood.
+                neighborhood[i] = solution.clone();
+                neighborhood[i][i] = neighborhood[i][i] == 1 ? 0 : 1;
 
-        // neighborWeight = 0;
-        // neighborValue = 0;
-        // for (int j = 0; j < neighborhood[i].length; j++) {
-        // neighborWeight += neighborhood[i][j] * weights[solutionDef[j]];
-        // neighborValue += neighborhood[i][j] * values[solutionDef[j]];
-        // neighborhoodData[i][0] = neighborWeight;
-        // neighborhoodData[i][1] = neighborValue;
-        // }
-        // }
+                // Get the value and weight of all the neighbors.
+                neighborWeight = 0;
+                neighborValue = 0;
+                for (int j = 0; j < neighborhood[i].length; j++) {
+                    neighborWeight += neighborhood[i][j] * data.get(j).get(1);
+                    neighborValue += neighborhood[i][j] * data.get(j).get(0);
+                }
+                neighborhoodData[i][0] = neighborValue;
+                neighborhoodData[i][1] = neighborWeight;
+            }
 
-        // solutionIsBest = true;
-        // for (int i = 0; i < solution.length; i++) {
-        // if (neighborhoodData[i][0] <= limit && neighborhoodData[i][1] >
-        // localBestValue) {
-        // solution = neighborhood[i].clone();
-        // localBestValue = neighborhoodData[i][1];
-        // solutionIsBest = false;
-        // System.out.println(Arrays.toString(solution));
-        // }
-        // }
-        // }
+            printNeighborhood(neighborhood, neighborhoodData);
 
-        // finalPrint(solution, localBestValue);
+            // See if any of the neighbors are better than the current solution.
+            solutionIsBest = true;
+            for (int i = 0; i < solution.length; i++) {
+                if (neighborhoodData[i][1] <= limit && neighborhoodData[i][0] > localBestValue) {
+                    solution = neighborhood[i].clone();
+                    localBestValue = neighborhoodData[i][0];
+                    solutionIsBest = false;
+                }
+            }
+        }
+
+        finalPrint(solution, localBestValue, data);
     }
 }
